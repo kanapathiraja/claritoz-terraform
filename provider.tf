9b9eff98d1fa2aae5d42b3ce1c0d0cf3cd0c8528
kanapathiraja <83782249+kanapathiraja@users.noreply.github.com>
2021-06-17 22:38:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,6 @@
 # Specify the provider and access details
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAVW72K7FCGH3LTZXD"
-  secret_key = "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"
+  access_key = ${{ secrets.AWS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRETE_ID }}
 }
