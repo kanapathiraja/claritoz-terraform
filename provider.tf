53fe1f6e30dab5934ce544f3d41afc141de36de1
kanapathiraja <83782249+kanapathiraja@users.noreply.github.com>
2021-06-17 22:14:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,5 +3,7 @@
 
 # Specify the provider and access details
 provider "aws" {
-  region                  = "us-west-2"
+  region     = "us-west-2"
+  access_key = "AKIAVW72K7FCGH3LTZXD"
+  secret_key = "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"
 }
